8695e0215344628112c8c2d4ed11fafde0263f34
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-09-04 12:13:03 +0300

    Add phpDocs

--- widgets/hello-world.php
+++ widgets/hello-world.php
@@ -15,30 +15,87 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  */
 class Hello_World extends Widget_Base {
 
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
 	public function get_name() {
 		return 'hello-world';
 	}
 
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
 	public function get_title() {
 		return __( 'Hello World', 'hello-world' );
 	}
 
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
 	public function get_icon() {
 		return 'eicon-posts-ticker';
 	}
 
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
 	public function get_categories() {
 		return [ 'general-elements' ];
 	}
 
 	/**
-	 * A list of scripts that the widgets is depended in
+	 * Retrieve the list of scripts the widget depended on.
+	 *
+	 * Used to set scripts dependencies required to run the widget.
+	 *
 	 * @since 1.3.0
-	 **/
+	 *
+	 * @access public
+	 *
+	 * @return array Widget scripts dependencies.
+	 */
 	public function get_script_depends() {
 		return [ 'hello-world' ];
 	}
 
+	/**
+	 * Register the controls of the widget.
+	 *
+	 * Adds different input fields to allow the user to change and customization widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
 	protected function _register_controls() {
 		$this->start_controls_section(
 			'section_content',
@@ -86,6 +143,15 @@ class Hello_World extends Widget_Base {
 		$this->end_controls_section();
 	}
 
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
 	protected function render() {
 		$settings = $this->get_settings();
 
@@ -94,6 +160,15 @@ class Hello_World extends Widget_Base {
 		echo '</div>';
 	}
 
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
 	protected function _content_template() {
 		?>
 		<div class="title">

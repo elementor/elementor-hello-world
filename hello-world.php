c94c561eb2ad3f1c556299a2a2f6605fd2b213b9
Kobi Zaltzberg <kobi@pojo.me>
2017-10-30 15:30:41 +0200

    1.1.0

--- hello-world.php
+++ hello-world.php
@@ -3,7 +3,7 @@
  * Plugin Name: Elementor Hello World
  * Description: Elementor sample plugin.
  * Plugin URI:  https://elementor.com/
- * Version:     1.0.0
+ * Version:     1.1.0
  * Author:      Author Name
  * Author URI:  https://elementor.com/
  * Text Domain: hello-world
